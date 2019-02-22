3414aead71264769b8d6707bdff57a69a7dec75d
luckysharma0408 <sharma1999lucky@gmail.com>
2019-02-22 09:22:44 +0530

    add var, head and size

--- src/main/SinglyLinkedList.java
+++ src/main/SinglyLinkedList.java
@@ -2,13 +2,23 @@ package main;
 
 public class SinglyLinkedList {
 
+    private Node head = null;
+    private int size = 0;
+
+
+
     private class Node{
     private int data;
     private Node next;
 
-        public Node(int data, Node next) {
+        private Node(int data, Node next) {
             this.data = data;
             this.next = next;
+        }
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
         }
     }
 
