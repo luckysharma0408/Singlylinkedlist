a6f760fad9dd322be2bd53c3dfdff33f309183e9
luckysharma0408 <sharma1999lucky@gmail.com>
2019-02-22 09:10:06 +0530

    add constructor for class

--- src/main/SinglyLinkedList.java
+++ src/main/SinglyLinkedList.java
@@ -6,6 +6,10 @@ public class SinglyLinkedList {
     private int data;
     private Node next;
 
+        public Node(int data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
     }
 
 }
